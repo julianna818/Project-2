adaf1221682bffcc0abf42e99c9242cb8119f451
julianna818 <jdoychinov@gmail.com>
2017-03-26 21:33:55 -0700

    3/26/17

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -3533,3 +3533,19 @@ var Popover = function ($) {
 }(jQuery);
 
 }();
+
+(function($) { // DOM Ready
+
+  // Insert all scripts here
+  $('nav ul li > a:not(:only-child)').click(function(e) {
+  $(this).siblings('.nav-dropdown').toggle();
+  $('.nav-dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+});
+ $('html').click(function() {
+      $('.nav-dropdown').hide();
+
+
+
+})(jQuery);
+
